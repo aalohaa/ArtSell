b06694710c7a1862ddc1e89f38f0493f357e05d0
Yuujiso <97373710+Yuujiso@users.noreply.github.com>
2022-04-25 09:16:50 +0600

    BottomNavbar

--- app/src/main/java/com/example/artsell/MainActivity.java
+++ app/src/main/java/com/example/artsell/MainActivity.java
@@ -6,10 +6,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.artsell.activities.HomeActivity;
+import com.example.artsell.databinding.ActivityMainBinding;
 import com.example.artsell.models.UserModel;
+import com.example.artsell.ui.category.CategoryFragment;
+import com.example.artsell.ui.home.HomeFragment;
+import com.example.artsell.ui.profile.ProfileFragment;
+import com.github.clans.fab.FloatingActionMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -20,6 +27,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -37,8 +47,13 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     FirebaseAuth auth;
-
     private AppBarConfiguration mAppBarConfiguration;
+
+    ActivityMainBinding binding;
+    BottomNavigationView navigationView;
+    FloatingActionMenu fabMenu;
+    com.github.clans.fab.FloatingActionButton fabProfile, fabCategory, fabOffers, fabNewProducts,fabMyCarts;
+    FloatingActionButton fabSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,44 +62,97 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        auth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_category, R.id.nav_profile, R.id.nav_offers, R.id.nav_new_products,
-                R.id.nav_my_orders, R.id.nav_my_carts)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        fabMenu = findViewById(R.id.fabMenu);
+        fabProfile = findViewById(R.id.nav_profile);
+        fabCategory = findViewById(R.id.nav_category);
+        fabOffers = findViewById(R.id.nav_offers);
+        fabNewProducts = findViewById(R.id.nav_new_products);
+        fabMyCarts = findViewById(R.id.nav_my_carts);
 
-        View headerView = navigationView.getHeaderView(0);
-        TextView headerName = headerView.findViewById(R.id.nav_header_name);
-        TextView headerEmail = headerView.findViewById(R.id.nav_header_email);
-        CircleImageView headerImg = headerView.findViewById(R.id.nav_header_img);
+        fabProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new ProfileFragment());
+                fabMenu.close( true );
+            }
+        });
+        fabCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View View) {
+                replaceFragment(new CategoryFragment());
+                fabMenu.close( true );
+            }
+        });
+        fabOffers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View View) {
+                replaceFragment(new OffersFragment());
+                fabMenu.close( true );
+            }
+        });
+        fabNewProducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View View) {
+                replaceFragment(new newProductsFragment());
+                fabMenu.close( true );
+            }
+        });
+        fabMyCarts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View View) {
+                replaceFragment(new MyCartsFragment());
+                fabMenu.close( true );
+            }
+        });
 
-        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        UserModel userModel = snapshot.getValue(UserModel.class);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.nav_my_orders:
+                    replaceFragment(new myOrdersFragment());
+                    break;
+                case R.id.scene_chat:
+                    replaceFragment(new ChatFragment());
+                    break;
+                case R.id.scene_btn:
 
-                        headerName.setText(userModel.getName());
-                        headerEmail.setText(userModel.getEmail());
-                        Glide.with(MainActivity.this).load(userModel.getProfileImg()).into(headerImg);
-                    }
+                    break;
+                default:
+                    return true;
+            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+            return true;
+        });
 
-                    }
-                });
+
+//        View headerView = navigationView.getHeaderView(0);
+//        TextView headerName = headerView.findViewById(R.id.nav_header_name);
+//        TextView headerEmail = headerView.findViewById(R.id.nav_header_email);
+//        CircleImageView headerImg = headerView.findViewById(R.id.nav_header_img);
+//
+//        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+//                .addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        UserModel userModel = snapshot.getValue(UserModel.class);
+//
+//                        headerName.setText(userModel.getName());
+//                        headerEmail.setText(userModel.getEmail());
+//                        Glide.with(MainActivity.this).load(userModel.getProfileImg()).into(headerImg);
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
     }
 
     @Override
@@ -99,5 +167,17 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.drawer_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    private void showToast(String message){
+        Toast.makeText( this, message, Toast.LENGTH_SHORT).show();
     }
 }
